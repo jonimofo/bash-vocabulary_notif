b5a84c3ac01127abccf7fe1810bedf2f46dddf69
jonimofo <benjamin.giralt@ynov.com>
2018-08-13 23:58:52 +0200

    trad et affichage notif fonctionnels

--- popup_voca.sh
+++ popup_voca.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-function get_random_line() {
+# TODO 
+# Check des packages zenity / translate shell
+# laisser une variable pour la langue ?
+# laisser la possibilité d'avoir la traduction complète ? (pas trouvé comment on fait encore)
+
+function get_random_line {
     line_count="$(wc -l ${vocab_file} | cut -d " " -f1)"
     random_line=$(shuf -i 1-$line_count -n1)
     echo $random_line
 }
 
-function get_word() {
+function get_word {
     # -n = quiet (suppress automatic printing of pattern space)
     # -p = print
     sed -n $1p ${vocab_file}
 }
 
-function display_popup() {
-    zenity --notification --text $1
+function translate {
+    trans -b :fr $1
+}
+
+function display_popup {
+    zenity --notification --timeout=3 --text "$1 \\n 
+$2"
     #TODO change time duration / color / brightness
 }
 
@@ -30,5 +40,7 @@ while [ -z "$random_word" ]; do
     random_word=$(get_word $random_line)
 done 
 
+translation=$(translate $random_word)
 
-display_popup $random_word 
+
+display_popup $random_word $translation  
