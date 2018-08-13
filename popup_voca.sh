970cec6994179dab97059e765faa60e035aceaf9
jonimofo <benjamin.giralt@ynov.com>
2018-08-13 21:41:23 +0200

    fonctionnel pour afficher notif depuis document texte

--- popup_voca.sh
+++ popup_voca.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+function get_random_line() {
+    line_count="$(wc -l ${vocab_file} | cut -d " " -f1)"
+    random_line=$(shuf -i 1-$line_count -n1)
+    echo $random_line
+}
 
-## Function
 function get_word() {
     # -n = quiet (suppress automatic printing of pattern space)
-    # -e = command to be executed
     # -p = print
     sed -n $1p ${vocab_file}
 }
 
-# Display popup  
 function display_popup() {
     zenity --notification --text $1
+    #TODO change time duration / color / brightness
 }
-
 
 
 ## Variables
 vocab_file="$HOME/Repos/popup-vocabulary/vocabulary.txt"
-line_count="$(wc -l ${vocab_file} | cut -d " " -f1)"
-rand_line=$(shuf -i 1-$line_count -n1)
-random_word=$(get_word $rand_line)
+random_line=$(get_random_line)
+random_word=$(get_word $random_line)
 
 
-display_popup $random_word
+# if an empty line is picked
+while [ -z "$random_word" ]; do
+    random_line=$(get_random_line)
+    random_word=$(get_word $random_line)
+done 
+
+
+display_popup $random_word 
